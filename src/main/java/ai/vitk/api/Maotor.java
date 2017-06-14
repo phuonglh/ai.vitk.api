308401dee8e6e8375e9068f27489159440a8cdc3
phuonglh <phuonglh@gmail.com>
2017-06-14 10:41:18 +0700

    Add maotor service

--- src/main/java/ai/vitk/api/Maotor.java
+++ src/main/java/ai/vitk/api/Maotor.java
@@ -1,8 +1,6 @@
 package ai.vitk.api;
 
 import ai.vitk.type.SAO;
-import ai.vitk.util.Mode;
-import ai.vitk.util.ModelParameters;
 import com.google.gson.Gson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,34 +9,27 @@ import io.swagger.annotations.ApiParam;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * @author Phuong LE-HONG, phuonglh@gmail.com
  * <p>
- * Mar 18, 2017, 2:45:39 PM
- * <p>
  * Subject, Attribute, Object extraction  service.
  *
  */
-@Path("/sao")
-@Api(value = "sao")
-public class Saotor {
-  private ai.vitk.sao.Saotor saotor;
+@Path("/mao")
+@Api(value = "mao")
+public class Maotor {
+  private ai.vitk.sao.Maotor maotor = new ai.vitk.sao.Maotor();
   private Gson gson = new Gson();
 	
-  public Saotor() {
-    ModelParameters parameters = new ModelParameters();
-    parameters.setLanguage("eng");
-    saotor = new ai.vitk.sao.Saotor(Mode.TEST, parameters);    
-  }
-  
 	@GET
 	@Path("/execute")
 	@ApiOperation(value = "English SAO Extractor")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response execute(@ApiParam(value = "text", required = true) @QueryParam("text") String text) {
-    SAO sao = saotor.decode(text);
+    List<SAO> sao = maotor.decode(text);
 		String json = gson.toJson(sao);
 		Response response = Response.status(200).entity(json).type(MediaType.APPLICATION_JSON).build();
 		return response;
